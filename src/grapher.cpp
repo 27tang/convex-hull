0a74cfb5308fdd31917e7e4da6f11974f4f3820a
Xuan <ytang@pdx.edu>
2015-11-21 22:43:30 -0800

    created the grapher utilitygit add -Agit add -Agit add -A

--- src/grapher.cpp
+++ src/grapher.cpp
@@ -1,32 +1,123 @@
 #include "point-line.h"
 #include <iostream>
 using namespace std;
+/*
+ *
+ * Grapher utility
+ *
+ * usage:
+ *        ./grapher [size of graph] < [text file created by point-set-generator]
+*/
 
-const int size = 20;
-
-void printGraph()
+class Grapher
 {
-	for(int i = 0; i < size + 1; ++i)
+ 	public:
+    	Grapher();
+        Grapher(int size);  //constructor specifying graph size
+   	 	~Grapher();
+
+    	void readInPoints();
+		int checkIfPointInSet(const Point & point);
+		void printGraph();
+ 
+	private:
+        int graphSize;
+		int setSize;
+		Point ** pointSet;
+
+};
+
+Grapher::Grapher()
+{
+	setSize = 0;
+	pointSet = NULL;
+}
+
+Grapher::Grapher(int size)
+{
+  setSize = 0;
+  pointSet = NULL;
+  graphSize = size;
+}
+
+Grapher::~Grapher(){
+
+}
+
+void Grapher::readInPoints()
+{
+  cin >> setSize; cin.ignore();
+
+  pointSet = new Point * [setSize];
+
+  for(int i = 0; i < setSize; ++i)
+  {
+    int x = 0;
+    int y = 0;
+    cin >> x; cin.ignore();
+    cin >> y; cin.ignore();
+    pointSet[i] = new Point(x, y);
+  }
+
+}
+
+int Grapher::checkIfPointInSet(const Point & point)
+{
+	for(int i = 0; i < setSize; ++i)
 	{
-		for(int j = 0; j < size + 1; ++j)
+		if(pointSet[i]->compareX(point)==0 && pointSet[i]->compareY(point)==0)
+			return 1;
+	}
+
+	return 0;
+
+}
+
+void Grapher::printGraph()
+{
+
+    Point * pointSetGraphed[setSize];
+    int printCursor = 0;
+    int count = 0;
+    for(int i = graphSize; i > -(graphSize + 1); --i)
+	{
+        int foundCount = 0;
+		for(int j = -graphSize; j < graphSize + 1; ++j)
 		{
-			if(i == size/2)
+            Point point(j, i);
+			if(checkIfPointInSet(point))
+            {
+                pointSetGraphed[count] = new Point(point);
+                ++count;
+                ++foundCount;
+				cout << "# ";
+            }
+			else if(i == 0)
 				cout << "+ ";
-			else if (j == size/2)
+			else if (j == 0)
 				cout << "+ ";
 			else
 				cout << ". ";
 		}
-		
+        for(int k = 0; k < foundCount; ++k)
+        {
+          pointSetGraphed[printCursor]->display();
+          cout << " ";
+          delete pointSetGraphed[printCursor];
+          ++printCursor;
+        }
 		cout << endl;
 	}
 }
 
 
 
-int main()
+int main(int argc, char * argv[])
 {
-	printGraph();
+    int graphSize = atoi(argv[1]); 
+	Grapher grapher(graphSize);
+	grapher.readInPoints();
+	grapher.printGraph();
 	return 0;
 }
 
