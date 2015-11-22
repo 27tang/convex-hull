46085a3eb75ba401c510e919e990c31698c1046b
Xuan <ytang@pdx.edu>
2015-11-21 20:37:59 -0800

    changed point-set-generator to be able to specify random range

--- src/point-set-generator.cpp
+++ src/point-set-generator.cpp
@@ -9,7 +9,7 @@ using namespace std;
 //    utility to generate a random set of points
 //  
 //    usage: 
-//      point-set-generator [size of set to generate] [output file name]
+//      point-set-generator [size of set to generate] [random num range] [output file name]
 
 int main(int argc, char * argv[])
 {
@@ -17,13 +17,14 @@ int main(int argc, char * argv[])
   srand(time(NULL));
 
   int count = atoi(argv[1]);
+  int range = atoi(argv[2]);
   ofstream outFile;
-  outFile.open(argv[2]);
+  outFile.open(argv[3]);
   outFile << count << endl;
 
   for(int i = 0; i < 2*count; ++i)
   {
-    outFile << rand() % 199 - 99 << endl;
+    outFile << (rand() % (range*2 + 1)) - range << endl;
   }
   outFile.close();
 
