df6c35bedc6fb04c868c1d219507a18d727a8f6c
Yu Shyuan Tang <ystang@stargate.cat.pdx.edu>
2015-11-28 16:44:10 -0800

    comboTester.sh has more going on now

--- src/comboTester.sh
+++ src/comboTester.sh
@@ -1,7 +1,7 @@
-./point-set-generator 10 10 -s > temp.in &&
-  cat temp.in | ./quick-hull -g | ./grapher 15 > quickRes.out &&
-  cat temp.in | ./brute-force -g  | ./grapher 15 > bruteRes.out && 
+./point-set-generator 20 20 -s > temp.in &&
+  cat temp.in | ./quick-hull -g | ./grapher 20 > quickRes.out &&
+  cat temp.in | ./brute-force -g  | ./grapher 20 > bruteRes.out && 
   cat temp.in | ./quick-hull >> quickRes.out &&
   cat temp.in | ./brute-force >> bruteRes.out && 
-  diff -y quickRes.out bruteRes.out -W 180
+  diff -y quickRes.out bruteRes.out -W 250
 
