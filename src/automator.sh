bdb4a945c8492c7bc1fbbdeb92bb62742d089c7a
Yu Shyuan Tang <ystang@stargate.cat.pdx.edu>
2015-12-01 23:00:56 -0800

    finished automator

---
+++ src/automator.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+numPoints=10
+
+program=$1
+
+echo ----------------------------------------------------------------
+echo TESTING $program. EACH OUTPUT IS THE AVERAGE CYCLES OF TEN RUNS
+echo ---------------------------------------------------------------- 
+
+function run {
+  for i in `seq 1 13`
+  do
+      cat temp.in | ./$program
+  done
+}
+
+for j in `seq 1 10`
+do
+ ./point-set-generator $numPoints 100 -s > temp.in
+  echo Running $program with $numPoints points...
+  run $numPoints | ./averager
+  numPoints=$(($numPoints*2))
+  
+
+done
