727eb73006fdff05ad2b0328e5c48fe424175322
Yu Shyuan Tang <ystang@stargate.cat.pdx.edu>
2015-12-02 21:59:27 -0800

    brooks stuff all fixed up

--- src/bComboTester.sh
+++ src/bComboTester.sh
@@ -6,7 +6,7 @@ screenSize=$(($range * 10 ))
 echo $screenSize
 
 ./point-set-generator $numPoints $range -s > temp.in &&
-  cat temp.in | ./brook-brute | ./grapher $range > quickRes.out &&
-  cat temp.in | ./brute-force -g  | ./grapher $range > bruteRes.out && 
+  cat temp.in | ./brook-quick | ./grapher $range > quickRes.out &&
+  cat temp.in | ./matt-quick -g  | ./grapher $range > bruteRes.out && 
   diff -y quickRes.out bruteRes.out -W "$screenSize"
 
